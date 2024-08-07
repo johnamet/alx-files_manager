9736f3f88db93e83e452733e24676bc72fc61353
johnny <johnametepeagboku@gmail.com>
2024-08-07 02:34:33 +0000

    Fixup: User registration API

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,24 +12,26 @@ class UsersController {
     const { email, password } = req.body;
 
     if (!email) {
-      res.status(400).send({ error: 'Missing email' });
+      return res.status(400).send({ error: 'Missing email' });
     }
 
     if (!password) {
-      res.status(400).send({ error: 'Missing password' });
+      return res.status(400).send({ error: 'Missing password' });
     }
 
     const existingUser = await dbClient.findUser({ email });
 
-    if (existingUser) {
-      res.status(400).send({ error: 'Already exists' });
+    console.log(existingUser);
+
+    if (existingUser.length !== 0) {
+      return res.status(400).send({ error: 'Already exists' });
     }
 
     const hashedPassword = hashPassword(password);
 
     const result = await dbClient.insertObject({ email, password: hashedPassword });
 
-    res.status(201).send({ email, id: result.insertedId });
+    return res.status(201).send({ email, id: result.insertedId });
   }
 }
 
