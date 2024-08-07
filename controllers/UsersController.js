d2f650c83f219584cb613df6acc0d20136da9298
johnny <johnametepeagboku@gmail.com>
2024-08-07 02:42:08 +0000

    Fixup: User registration API

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,9 +4,9 @@ import dbClient from '../utils/db';
 function hashPassword(password) {
   const hash = crypto.createHash('sha1');
   hash.update(password);
-
   return hash.digest('hex');
 }
+
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
@@ -19,17 +19,21 @@ class UsersController {
       return res.status(400).send({ error: 'Missing password' });
     }
 
-    const existingUser = await dbClient.findUser({ email });
+    try {
+      const existingUser = await dbClient.findUser({ email });
 
-    if (existingUser.length !== 0) {
-      return res.status(400).send({ error: 'Already exists' });
+      if (existingUser.length > 0) {
+        return res.status(400).send({ error: 'Already exists' });
+      }
+
+      const hashedPassword = hashPassword(password);
+      const result = await dbClient.insertObject({ email, password: hashedPassword });
+
+      return res.status(201).send({ email, id: result.insertedId });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({ error: 'Internal server error' });
     }
-
-    const hashedPassword = hashPassword(password);
-
-    const result = await dbClient.insertObject({ email, password: hashedPassword });
-
-    return res.status(201).send({ email, id: result.insertedId });
   }
 }
 
